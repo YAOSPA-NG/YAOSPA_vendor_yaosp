7078326e8bd216cb27aaa116b00cd19cdc74050a
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-09-19 00:09:45 +0200

    YAOSPA-NG v1.10
      - More - Mobile networks : Use small/big roaming indicator in statusbar
      - Battery : reorder menu items (move Battery optimisation to action, Battery style into menu)
      - Disabled safe headphone volume
      - Add complete Sept. Patch as extracted from 8.0.0 by LineageOS
      - Fix all kernels (CVE-2017-1000251, part of BlueBorne vulnerability fix)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- configs/yaosp_main.mk
+++ configs/yaosp_main.mk
@@ -23,10 +23,10 @@ PRODUCT_COPY_FILES += \
 
 # Add YAOSPA version
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.yaosp.version=1.02 \
+    ro.yaosp.version=1.10 \
     ro.yaosp.branch=7.1.2 \
     ro.yaosp.buildbase=NZH54D \
-    ro.yaosp.builddate=2017-09-10
+    ro.yaosp.builddate=2017-09-18
 
 # No asssitant for us, please
 PRODUCT_PROPERTY_OVERRIDES += \
