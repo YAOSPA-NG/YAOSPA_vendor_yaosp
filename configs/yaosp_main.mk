e82f62985c8625920df724ab98d6f8eb9d4fab77
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-10-14 23:39:41 +0200

    YAOSPA-NG v1.20
      - rebased on AOSPA 7.3.0 / 7.1.2 NZH54D (synced on 20170911 2333 CET)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- configs/yaosp_main.mk
+++ configs/yaosp_main.mk
@@ -27,10 +27,10 @@ PRODUCT_COPY_FILES += \
 
 # Add YAOSPA version
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.yaosp.version=1.10 \
+    ro.yaosp.version=1.20 \
     ro.yaosp.branch=7.1.2 \
     ro.yaosp.buildbase=NZH54D \
-    ro.yaosp.builddate=2017-09-18
+    ro.yaosp.builddate=2017-10-12
 
 # No asssitant for us, please
 PRODUCT_PROPERTY_OVERRIDES += \
