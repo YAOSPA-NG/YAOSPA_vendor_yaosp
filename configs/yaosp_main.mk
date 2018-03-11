a2515337c22f5f11bc09bf56458dbad34e3acd14
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2018-03-11 18:03:24 +0100

    YAOSPA-NG v1.41
      - Add support for Android P's preview "edit" action in screenshot notification
        (get Google's modded Markup apk from the xda thread :
         https://forum.xda-developers.com/android/apps-games/app-google-markup-android-p-arm64-t3760345)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- configs/yaosp_main.mk
+++ configs/yaosp_main.mk
@@ -27,10 +27,10 @@ PRODUCT_COPY_FILES += \
 
 # Add YAOSPA version
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.yaosp.version=1.40 \
+    ro.yaosp.version=1.41 \
     ro.yaosp.branch=7.1.2 \
     ro.yaosp.buildbase=NZH54D \
-    ro.yaosp.builddate=2018-03-09
+    ro.yaosp.builddate=2018-03-11
 
 # No asssitant for us, please
 PRODUCT_PROPERTY_OVERRIDES += \
